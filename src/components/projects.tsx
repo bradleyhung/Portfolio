4bb6e0474392fe47eaf2f5ed119847b2c84f5d29
bradleyhung <bradleyhung5570@gmail.com>
2025-02-24 00:52:27 -0500

    Changed projects.tsx colors

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -8,24 +8,53 @@ import Link from "next/link";
 const projects = [
   {
     title: "CampusQuest",
-    description: "An innovative web application designed to foster student connections through customizable social activities. Whether it's a study session at the library, a casual lunch hangout, or an educational trip to the museum, students can create and share their own unique \"Social Quests.\" This platform encourages collaboration, friendship, and enriching experiences, making campus life more engaging and enjoyable.",
+    description:
+      'An innovative web application designed to foster student connections through customizable social activities. Whether it\'s a study session at the library, a casual lunch hangout, or an educational trip to the museum, students can create and share their own unique "Social Quests." This platform encourages collaboration, friendship, and enriching experiences, making campus life more engaging and enjoyable.',
     image: "/assets/CampusQuestImage.jpg",
-    tags: ["Express.js", "Firebase", "React", "MERN Stack", "JavaScript", "Node.js", "TailwindCSS"],
+    tags: [
+      "Express.js",
+      "Firebase",
+      "React",
+      "MERN Stack",
+      "JavaScript",
+      "Node.js",
+      "TailwindCSS",
+    ],
     liveUrl: "https://campus-quest.vercel.app/",
   },
   {
     title: "PetPals",
-    description: "A web application that will allow users without pets to explore various pet species and connect with current pet owners, fostering a sense of community and shared interest. Additionally, it will serve as a social media hub where pet owners can communicate, bond, and invite non-pet owners to join their activities.",
+    description:
+      "A web application that will allow users without pets to explore various pet species and connect with current pet owners, fostering a sense of community and shared interest. Additionally, it will serve as a social media hub where pet owners can communicate, bond, and invite non-pet owners to join their activities.",
     image: "/assets/PetpalImage.jpg",
-    tags: ["NextJS", "Express.js", "MongoDB", "TailwindCSS", "Google Gemini", "Retrieval-Augmented Generation (RAG)", "Node.js", "Git", "TypeScript", "Amazon S3"],
+    tags: [
+      "NextJS",
+      "Express.js",
+      "MongoDB",
+      "TailwindCSS",
+      "Google Gemini",
+      "Retrieval-Augmented Generation (RAG)",
+      "Node.js",
+      "Git",
+      "TypeScript",
+      "Amazon S3",
+    ],
     githubUrl: "https://github.com/birongliu/PetPals",
     liveUrl: "https://cisc-4900.vercel.app/",
   },
   {
     title: "ScholarshipFinder",
-    description: "A dedicated platform designed exclusively for CUNY students to discover and apply for scholarships. Our mission is to empower students by providing a streamlined, user-friendly experience to access financial aid opportunities tailored to their unique profiles",
+    description:
+      "A dedicated platform designed exclusively for CUNY students to discover and apply for scholarships. Our mission is to empower students by providing a streamlined, user-friendly experience to access financial aid opportunities tailored to their unique profiles",
     image: "/assets/ScholarshipImage.jpg",
-    tags: ["MongoDB", "TypeScript", "Express.js", "React.js", "Node.js", "MERN Stack"],
+    tags: [
+      "MongoDB",
+      "TypeScript",
+      "Express.js",
+      "React.js",
+      "Node.js",
+      "MERN Stack",
+    ],
     githubUrl: "https://github.com/XiuwenZ/ScholarshipFinder",
   },
 ];
@@ -51,7 +80,7 @@ export function Projects() {
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{ once: true }}
               transition={{ delay: index * 0.2 }}
-              className="group relative bg-white rounded-lg shadow-lg overflow-hidden"
+              className="group relative bg-blue rounded-lg shadow-lg overflow-hidden"
             >
               <div className="relative h-48 md:h-64">
                 <Image
@@ -68,7 +97,7 @@ export function Projects() {
                   {project.tags.map((tag) => (
                     <span
                       key={tag}
-                      className="px-3 py-1 bg-gray-100 text-sm rounded-full"
+                      className="px-3 py-1 bg-blue-300 text-sm rounded-full"
                     >
                       {tag}
                     </span>
